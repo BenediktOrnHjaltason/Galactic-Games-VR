23af8cddfb8a736d82b8cf6a854eb16b5df4b457
BenediktOrnHjaltason <bendikorn@gmail.com>
2021-01-31 21:24:50 +0100

    Fleshed out structures on rails concept

--- Assets/PROJECT/Structures/Controllable/On rails/StructureOnRails.cs
+++ Assets/PROJECT/Structures/Controllable/On rails/StructureOnRails.cs
@@ -30,14 +30,27 @@ public class StructureOnRails : MonoBehaviour
 
         switch(selfAttractDirection)
         {
+            case EPlatformMoveGlobal.X_Positive:
+                moveForce = new Vector3(5, 0, 0);
+                RB.constraints = RigidbodyConstraints.FreezePositionY | RigidbodyConstraints.FreezePositionZ |
+                    RigidbodyConstraints.FreezeRotation;
+                break;
+
+            case EPlatformMoveGlobal.X_Negative:
+                moveForce = new Vector3(-5, 0, 0);
+                RB.constraints = RigidbodyConstraints.FreezePositionY | RigidbodyConstraints.FreezePositionZ |
+                    RigidbodyConstraints.FreezeRotation;
+                break;
+
+
             case EPlatformMoveGlobal.Y_Positive:
-                moveForce = new Vector3(0, 100, 0);
+                moveForce = new Vector3(0, 5, 0);
                 RB.constraints = RigidbodyConstraints.FreezePositionZ | RigidbodyConstraints.FreezePositionX |
                     RigidbodyConstraints.FreezeRotation;
                 break;
 
             case EPlatformMoveGlobal.Y_Negative:
-                moveForce = new Vector3(0, -100, 0);
+                moveForce = new Vector3(0, -5, 0);
                 RB.constraints = RigidbodyConstraints.FreezePositionZ | RigidbodyConstraints.FreezePositionX |
                     RigidbodyConstraints.FreezeRotation;
                 break;
@@ -50,7 +63,7 @@ public class StructureOnRails : MonoBehaviour
     void FixedUpdate()
     {
         if (realtime.connected && realtimeRtt.ownerIDSelf == realtime.clientID)
-        RB.AddForce(moveForce);
+        RB.AddForce(moveForce * 40 * Time.deltaTime);
     }
 
 }
