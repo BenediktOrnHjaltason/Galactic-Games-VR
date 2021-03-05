4bf666051b0f064d5d99299ad4e6a0d5faae71d1
BenediktOrnHjaltason <bendikorn@gmail.com>
2021-03-05 14:44:09 +0100

    Attractor Rift beams to players in reach

--- Assets/PROJECT/Hazards/Attractor Rift/AttractorRift_PlayerSensor.cs
+++ Assets/PROJECT/Hazards/Attractor Rift/AttractorRift_PlayerSensor.cs
@@ -18,8 +18,6 @@ public class AttractorRift_PlayerSensor : MonoBehaviour
     [SerializeField]
     AttractorRift_Core core;
 
-    GameObject defaultLinesBase;
-
     Transform attractionPoint;
     RealtimeTransform rtt;
     Rigidbody rb;
@@ -32,14 +30,17 @@ public class AttractorRift_PlayerSensor : MonoBehaviour
     float changeInterval = 0.166f;
     float nextTimeToChange = 0;
     float timeAtChange = 0;
+    GameObject dummyObject;
 
     //--------- Default beams
-
     List<LineRenderer> defaultBeams = new List<LineRenderer>();
 
-    Vector3 randomPathToEdge;
-    Vector3 randomDirection;
-    GameObject dummyObject;
+    Vector3 pathToEdge;
+    Vector3 direction;
+
+    //--------- PlayerLines
+    List<LineRenderer> playerBeams = new List<LineRenderer>();
+    
 
 
 
@@ -63,38 +64,65 @@ public class AttractorRift_PlayerSensor : MonoBehaviour
         defaultBeams.Add(transform.GetChild(0).transform.GetChild(2).GetComponent<LineRenderer>());
         defaultBeams.Add(transform.GetChild(0).transform.GetChild(3).GetComponent<LineRenderer>());
 
+        playerBeams.Add(transform.GetChild(1).transform.GetChild(0).GetComponent<LineRenderer>());
+        playerBeams.Add(transform.GetChild(1).transform.GetChild(1).GetComponent<LineRenderer>());
+        playerBeams.Add(transform.GetChild(1).transform.GetChild(2).GetComponent<LineRenderer>());
+
     }
 
     private void FixedUpdate()
     {
-        //HandleDefaultBeams
+        //Handle beams
 
         if (Time.time > nextTimeToChange)
         {
             nextTimeToChange += changeInterval;
 
+            //Default beams 
             foreach (LineRenderer beam in defaultBeams)
             {
                 beam.SetPosition(0, attractionPoint.transform.position);
 
-                randomDirection = new Vector3(Random.Range(-1.0f, 1.0f), Random.Range(-1.0f, 1.0f), Random.Range(-1.0f, 1.0f));
-                randomPathToEdge = randomDirection.normalized * transform.localScale.x / 2;
+                direction = new Vector3(Random.Range(-1.0f, 1.0f), Random.Range(-1.0f, 1.0f), Random.Range(-1.0f, 1.0f));
+                pathToEdge = direction.normalized * transform.localScale.x / 2;
 
 
-                dummyObject.transform.rotation = Quaternion.LookRotation(randomPathToEdge * 10);
+                dummyObject.transform.rotation = Quaternion.LookRotation(pathToEdge * 10);
 
-                beam.SetPosition(1, attractionPoint.transform.position + (randomPathToEdge / 2) +
-                    dummyObject.transform.up * randomDirection.x +
-                    dummyObject.transform.right * randomDirection.y);
+                beam.SetPosition(1, attractionPoint.transform.position + (pathToEdge / 2) +
+                    dummyObject.transform.up * direction.x +
+                    dummyObject.transform.right * direction.y);
 
-                beam.SetPosition(2, attractionPoint.transform.position + randomPathToEdge);
+                beam.SetPosition(2, attractionPoint.transform.position + pathToEdge);
             }
+
+            //Beams to players
+            if (playersInReach.Count > 0)
+            {
+                for (int i = 0; i < playersInReach.Count; i++)
+                {
+                    playerBeams[i].SetPosition(0, transform.position);
+
+                    direction = playersInReach[i].transform.position - transform.position;
+
+                    playerBeams[i].SetPosition(1, transform.position + (direction / 2) +
+                    dummyObject.transform.up * (direction.x /5) +
+                    dummyObject.transform.right * (direction.y /5));
+
+                    playerBeams[i].SetPosition(2, transform.position + direction);
+                }
+            }
+
+            else 
+                foreach (LineRenderer beam in playerBeams)
+                {
+                    beam.SetPosition(0, transform.position);
+                    beam.SetPosition(1, transform.position);
+                    beam.SetPosition(2, transform.position);
+                }
         }
 
-        else
-        {
-
-        }
+        
 
         if (!rtt.realtime.connected) return;
 
