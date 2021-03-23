05a34d3cfb17fb314af0ed6fe8b706170b06d3b1
BenediktOrnHjaltason <bendikorn@gmail.com>
2021-03-23 02:20:30 +0100

    Dyson pieces connect with beams and gather energy. Portal to main menu is spawned when energy full

---
+++ Assets/PROJECT/Puzzles/EndPuzzles/Dyson Sphere/Pieces/DysonSpherePiece.cs
@@ -0,0 +1,146 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Normal.Realtime;
+using System;
+
+public class DysonSpherePiece : RealtimeComponent<DysonSpherePiece_Model>
+{
+    [SerializeField]
+    LineRenderer beamToStar;
+
+    [SerializeField]
+    Material searchMaterial;
+
+    [SerializeField]
+    Material connectedMaterial;
+
+    [SerializeField]
+    RealtimeTransform rtt;
+
+    public RealtimeTransform Rtt { get => rtt; }
+
+    float starBeamLength = 17.5f;
+
+
+    static bool bothPiecesConnectedToStar = false;
+
+
+    [SerializeField]
+    DysonSpherePiece otherPiece;
+
+    [SerializeField]
+    LineRenderer connectorBeamToOtherPiece;
+
+    [SerializeField]
+    GameObject otherPieceConnectionReceiver;
+
+    [SerializeField]
+    Material zeroLoadMaterial;
+
+    [SerializeField]
+    Material fullLoadMaterial;
+
+    static float energyExtractionIncrement = 0;
+    static bool energyExtractionComplete = false;
+
+    public event Action OnEnergyExtractionComplete;
+
+    RaycastHit structureHit;
+
+    public event Action OnConnectedToStar;
+
+
+    private void FixedUpdate()
+    {
+        if (Physics.Raycast(transform.position, transform.forward, starBeamLength / 2, 1 << 15))
+        {
+            if (!connectedToStar)
+            {
+                ConnectedToStar = true;
+                OnConnectedToStar?.Invoke();
+            }
+        }
+
+        else if (connectedToStar) ConnectedToStar = false;
+
+        beamToStar.SetPosition(0, transform.position);
+        beamToStar.SetPosition(1, transform.position + transform.forward * starBeamLength);
+
+        if (bothPiecesConnectedToStar)
+        {
+            connectorBeamToOtherPiece.SetPosition(0, connectorBeamToOtherPiece.transform.position);
+            connectorBeamToOtherPiece.SetPosition(1, otherPieceConnectionReceiver.transform.position);
+
+            if (!energyExtractionComplete && energyExtractionIncrement < 1)
+            {
+                energyExtractionIncrement += 0.001f;
+
+                connectorBeamToOtherPiece.material.Lerp(zeroLoadMaterial, fullLoadMaterial, energyExtractionIncrement);
+            }
+            else if (!energyExtractionComplete)
+            {
+                energyExtractionComplete = true;
+                OnEnergyExtractionComplete?.Invoke();
+            }
+        }
+
+        else
+        {
+            connectorBeamToOtherPiece.SetPosition(0, connectorBeamToOtherPiece.transform.position);
+            connectorBeamToOtherPiece.SetPosition(1, connectorBeamToOtherPiece.transform.position);
+        }
+    }
+
+    //Networking
+
+    protected override void OnRealtimeModelReplaced(DysonSpherePiece_Model previousModel, DysonSpherePiece_Model currentModel)
+    {
+        if (previousModel != null)
+        {
+            // Unregister from events
+            previousModel.connectedToStarDidChange -= ConnectedToStarDidChange;
+        }
+
+        if (currentModel != null)
+        {
+            // If this is a model that has no data set on it, populate it with the current availability value.
+            if (currentModel.isFreshModel)
+            {
+                currentModel.connectedToStar = connectedToStar;
+            }
+
+            //Update data to match the new model
+            UpdateConnectedToStar();
+
+            // Register for events so we'll know if data changes later
+            currentModel.connectedToStarDidChange += ConnectedToStarDidChange;
+        }
+    }
+
+    bool connectedToStar = false;
+
+    public bool ConnectedToStar { get => connectedToStar; set => model.connectedToStar = value; }
+
+    void ConnectedToStarDidChange(DysonSpherePiece_Model model, bool connected)
+    {
+        UpdateConnectedToStar();
+    }
+
+    void UpdateConnectedToStar()
+    {
+        connectedToStar = model.connectedToStar;
+
+        beamToStar.material = (connectedToStar) ? connectedMaterial : searchMaterial;
+
+        if (connectedToStar && otherPiece.ConnectedToStar)
+            bothPiecesConnectedToStar = true;
+
+        else
+        {
+            bothPiecesConnectedToStar = false;
+            energyExtractionIncrement = 0.0f;
+            connectorBeamToOtherPiece.material = zeroLoadMaterial;
+        }
+    }
+}
--- Assets/PROJECT/Puzzles/EndPuzzles/Dyson Sphere/Puzzle_DysonSphere.cs
+++ Assets/PROJECT/Puzzles/EndPuzzles/Dyson Sphere/Puzzle_DysonSphere.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using System;
+using UnityEngine.SceneManagement;
 
 public class Puzzle_DysonSphere : MonoBehaviour
 {
@@ -9,7 +10,7 @@ public class Puzzle_DysonSphere : MonoBehaviour
     Star_Pulsar star;
 
     [SerializeField]
-    float starAttractionForce = 30000.0f;
+    float starAttractionForce = 590000.0f;
 
 
     [SerializeField]
@@ -29,15 +30,31 @@ public class Puzzle_DysonSphere : MonoBehaviour
 
     List<StructureSync> piecesInsideMagneticField = new List<StructureSync>();
 
+    [SerializeField]
+    GeneralTrigger portalTrigger;
+
+    [SerializeField]
+    GameObject portal;
+
+    Vector3 portalEndScale = new Vector3(2.015517f, 6.777942f, 2.015517f);
+
 
     // Start is called before the first frame update
     void Start()
     {
-        leftPieceStartLocalPos = leftDysonPiece.transform.localPosition;
-        leftPieceStartLocalRot = leftDysonPiece.transform.localRotation;
+        if (leftDysonPiece && rightDysonPiece)
+        {
+            leftPieceStartLocalPos = leftDysonPiece.transform.localPosition;
+            leftPieceStartLocalRot = leftDysonPiece.transform.localRotation;
 
-        rightPieceStartLocalPos = rightDysonPiece.transform.localPosition;
-        rightPieceStartLocalRot = rightDysonPiece.transform.localRotation;
+            rightPieceStartLocalPos = rightDysonPiece.transform.localPosition;
+            rightPieceStartLocalRot = rightDysonPiece.transform.localRotation;
+
+            leftDysonPiece.OnEnergyExtractionComplete += ReleaseLevelEndPortal;
+            rightDysonPiece.OnEnergyExtractionComplete += ReleaseLevelEndPortal;
+        }
+
+        
 
         if (star)
         {
@@ -49,25 +66,42 @@ public class Puzzle_DysonSphere : MonoBehaviour
             magneticFieldTrigger.OnEnteredTrigger += RegisterDysonPieceEnteredMagneticField;
             magneticFieldTrigger.OnExitedTrigger += RegisterDysonPieceExitedMagneticField;
         }
+
+        if (portalTrigger)
+        {
+            portalTrigger.OnEnteredTrigger += SendPlayerToMainMenu;
+        }
     }
 
     private void FixedUpdate()
     {
         foreach (StructureSync ss in piecesInsideMagneticField)
         {
-            ss.AddGravityForce((star.transform.position - ss.transform.position).normalized * starAttractionForce);
+            if (ss.Rtt.ownerIDSelf == ss.Rtt.realtime.clientID)
+                    ss.AddGravityForce((star.transform.position - ss.transform.position).normalized * starAttractionForce);
+        }
+
+        if (levelEndPortalReleased)
+        {
+            if (portalIncrement < 1.0f)
+            {
+                portalIncrement += 0.02f;
+
+                portal.transform.localScale = Vector3.Lerp(Vector3.zero, portalEndScale, portalIncrement);
+            }
         }
     }
 
     void ResetDysonPieceToStart(DysonSpherePiece dysonPiece)
     {
-        if (dysonPiece.Rtt.realtime.clientID == dysonPiece.Rtt.ownerIDSelf)
-        {
-            StructureSync ss = dysonPiece.GetComponent<StructureSync>();
+        StructureSync ss = dysonPiece.GetComponent<StructureSync>();
 
-            if (ss)
+        if (ss)
+        {
+            if (piecesInsideMagneticField.Contains(ss)) piecesInsideMagneticField.Remove(ss);
+
+            if (dysonPiece.Rtt.realtime.clientID == dysonPiece.Rtt.ownerIDSelf)
             {
-                if (piecesInsideMagneticField.Contains(ss)) piecesInsideMagneticField.Remove(ss);
                 ss.ResetLinearVelocity();
                 ss.BreakControl();
                 dysonPiece.transform.localPosition = (dysonPiece == leftDysonPiece) ? leftPieceStartLocalPos : rightPieceStartLocalPos;
@@ -84,8 +118,8 @@ public class Puzzle_DysonSphere : MonoBehaviour
         {
             StructureSync ss = dysonPiece.GetComponent<StructureSync>();
 
-            if (ss && ss.Rtt.realtime.clientID == ss.Rtt.ownerIDSelf && !piecesInsideMagneticField.Contains(ss))
-                piecesInsideMagneticField.Add(ss);
+            if (!piecesInsideMagneticField.Contains(ss))
+                    piecesInsideMagneticField.Add(ss);
         }
     }
 
@@ -97,8 +131,29 @@ public class Puzzle_DysonSphere : MonoBehaviour
         {
             StructureSync ss = dysonPiece.GetComponent<StructureSync>();
 
-            if (ss && ss.Rtt.realtime.clientID == ss.Rtt.ownerIDSelf && piecesInsideMagneticField.Contains(ss))
-                piecesInsideMagneticField.Remove(ss);
+            if (piecesInsideMagneticField.Contains(ss))
+                    piecesInsideMagneticField.Remove(ss);
         }
+    }
+
+    bool levelEndPortalReleased = false;
+    float portalIncrement = 0.0f;
+
+    void ReleaseLevelEndPortal()
+    {
+        if (!levelEndPortalReleased)
+        {
+            levelEndPortalReleased = true;
+
+            leftDysonPiece.OnEnergyExtractionComplete -= ReleaseLevelEndPortal;
+            rightDysonPiece.OnEnergyExtractionComplete -= ReleaseLevelEndPortal;
+        }
+    }
+
+    void SendPlayerToMainMenu(Collider other)
+    {
+        OVRPlayerController pc = other.GetComponent<OVRPlayerController>();
+        
+        if (pc) SceneManager.LoadScene("MainMenu");
     }
 }
