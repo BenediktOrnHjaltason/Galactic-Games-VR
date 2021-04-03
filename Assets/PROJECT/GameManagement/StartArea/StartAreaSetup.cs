55fe4bec78a73e77101f2a4adfb2027356ef7c3c
BenediktOrnHjaltason <bendikorn@gmail.com>
2021-04-03 13:38:07 +0200

    Turning off spatialization for player voices for a radio contact feel

---
+++ Assets/PROJECT/GameManagement/StartArea/StartAreaSetup.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartAreaSetup : MonoBehaviour
+{
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.layer.Equals(14))
+        {
+            AudioSource audioSource = other.GetComponent<AudioSource>();
+
+            if (audioSource && audioSource.spatialize)
+            {
+                audioSource.spatialize = false;
+                Debug.Log("spatialization turned off for player audio source");
+            }
+        }
+    }
+
+}
