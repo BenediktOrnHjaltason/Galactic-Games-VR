de3283d848633e7146036f54d3de698a0f4d696f
BenediktOrnHjaltason <bendikorn@gmail.com>
2021-04-08 11:45:46 +0200

    Player is moved along by horizontal moving rails platforms

---
+++ Assets/PROJECT/Structures/Controllable/RestrictedMovement/PlayerFriction.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerFriction : MonoBehaviour
+{
+    Rigidbody platformRB;
+
+    OVRPlayerController player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        platformRB = transform.root.GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        if (player)
+        {
+            player.Controller.Move(platformRB.velocity / 50);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.layer.Equals(14))
+        {
+            player = other.GetComponent<OVRPlayerController>();
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.layer.Equals(14))
+        {
+            OVRPlayerController ovrpc = other.GetComponent<OVRPlayerController>();
+
+            if (ovrpc = player) player = null;
+        }
+    }
+}
